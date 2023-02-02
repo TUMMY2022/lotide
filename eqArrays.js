82892138f17512379aedc36abe9da161520774ab
Tomy <to88my@gmail.com>
2023-02-02 16:44:15 +0000

    Implement eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,18 @@
 // FUNCTION IMPLEMENTATION
-const eqArrays = function(array1, array2){
-  if(array1.length !== array2.length) {
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
     return false;
-  } else { 
+  } else {
     for (let j = 0; j < array1.length; j++) {
       if (Array.isArray(array1[j])) {
-        return eqArrays(array1[j], array2[j])
-      } else if ( array1[j] !== array2[j]) {
+        return eqArrays(array1[j], array2[j]);
+      } else if (array1[j] !== array2[j]) {
         return false;
       }
     }
     return true;
   }
-} 
+};
 
 const assertEqual = function(actual, expected) {
   if (actual === expected)
